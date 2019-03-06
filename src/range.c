d8cbd04b10179bdff21345809bfa68cd58d3a731
Nicolas Denoyelle <ndenoyelle@anl.gov>
2019-03-06 13:49:43 -0600

    [ci] Adding CI, fixing authors, license.

--- src/range.c
+++ src/range.c
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright 2019 UChicago Argonne, LLC.
+ * (c.f. AUTHORS, LICENSE)
+ *
+ * This file is part of the EXCIT project.
+ * For more info, see https://xgitlab.cels.anl.gov/argo/excit
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
 #include "dev/excit.h"
 #include "range.h"
 
